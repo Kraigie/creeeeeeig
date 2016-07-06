133ef48151568cfc7870da70d26f087a2b458c7b
Craig Dazey <craigdazey@gmail.com>
2016-07-06 16:10:31 -0400

    added eval command

---
+++ commands/eval.js
@@ -0,0 +1,37 @@
+const config = require('../config');
+const now = require('performance-now')
+const util = require('util');
+
+bot.registerCommand('eval', (msg, args) => {
+    let before = now();
+
+    try {
+        let evald = eval(args.join(' '));
+        if(evald.length > 1000)
+            evald = evald.substring(0, 1000);
+        let after = now();
+        let retStr = `\`\`\`javascript\n` +
+
+        `Input: ${args.join(' ')}\n` +
+        `Output: ${evald}\n` +
+        `Time: ${(after - before).toFixed(3)} ms\`\`\``;
+
+        return retStr;
+    } catch (err) {
+        let after = now();
+        let retStr = `\`\`\`javascript\n` +
+
+        `Input: ${args.join(' ')}\n` +
+        `Error: ${err}\n` +
+        `Time: ${(after - before).toFixed(3)} ms\`\`\``;
+
+        return retStr;
+    }
+}, {
+    description: 'Eval things',
+    fullDescription: 'The bot will attempt to evaluate a given expression - limited to bot creator id',
+    usage: '<expression>',
+    requirements: {
+        userIDs: [config.evalid]
+    }
+});
