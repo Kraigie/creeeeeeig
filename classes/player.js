27cc0dbccec87e28b9de2c8488b842550d9dd989
Craig Dazey <craigdazey@gmail.com>
2016-07-13 11:08:30 -0400

    renamed queue class, added some functionality

---
+++ classes/player.js
@@ -0,0 +1,36 @@
+'use strict;'
+
+module.exports = class Player {
+    constructor(conn, chann) {
+        this.conn = conn;
+        this.chann = chann;
+        this.queue = [];
+
+        conn.on('end', this.playNext());
+    }
+
+    addSong(song) {
+        if(this.queue.length == 0 && !this.conn.playing) {
+            this.queue.push(song);
+            this.queue.playNext();
+        }
+        else {
+            this.queue.push(song);
+            bot.createMessage(chann.id, `Your song was added to the queue in position ${this.queue.length}`);
+        }
+    }
+
+    playNext() {
+        let next = this.queue.shift();
+
+        if(next) {
+            next.getStream().then(stream => {
+                conn.playStream(stream);
+            })
+        }
+    }
+
+    skip() {
+        conn.stopPlaying();
+    }
+}
