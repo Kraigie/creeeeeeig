9feaada30d143429c2d9403e9fda020485edb350
Craig Dazey <craigdazey@gmail.com>
2016-07-14 17:30:01 -0400

    added mute command

--- commands/admin.js
+++ commands/admin.js
@@ -4,7 +4,26 @@ const config = require('../config');
 //TODO: Get the purge command fixed so it deletes the proper amount
 //TODO: Clean them case sensitivity
 //TODO: Clean them nicknames
-//TODO: Should userIDs override the rest of the permissions?
+//TODO: Should userIDs override the rest of the permissions? I think so
+
+bot.registerCommand('mute', (msg, args) => {
+    if(args.length === 0) return 'Please supply a user to mute';
+
+    let toFind = bot.users.find(u => u.username === args[0]);
+    if(!toFind) return 'Please supply a valid user';
+
+    bot.editChannelPermissions(msg.channel.id, msg.author.id, 0, 2048);
+}, {
+    description: 'Deletes messages',
+    fullDescription: 'The bot will delete messages from all users in the last specified number of messages',
+    usage: '<number to search through>',
+    requirements: {
+        userIDs: [config.evalid],
+        permissions: {
+            "manageMessages": true
+        }
+    }
+});
 
 let clean = bot.registerCommand('clean', (msg, args) => {
     if(args.length === 0) return 'Please supply a number of messages';
